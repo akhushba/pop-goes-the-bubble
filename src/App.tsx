3a0bc432dc7b164f2a780bd66c8a9cbf84edd099
Amna Khushbakht <113641076+akhushba@users.noreply.github.com>
2025-04-03 14:56:52 -0400

    page changes + references

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import { useState } from "react";
-import Guide from "./components/Guide";
+import { Outlet } from "react-router-dom";
 import { MenuDrawer } from "./components/MenuDrawer";
-import Simulation from "./components/Simulation";
 
 function App() {
-  const [guideIsActive, setGuideIsActive] = useState(true);
-  const [simulationIsActive, setSimulationIsActive] = useState(false);
-
   return (
     <>
-      <MenuDrawer
-        setGuideIsActive={setGuideIsActive}
-        setSimulationIsActive={setSimulationIsActive}
-      />
-      {guideIsActive && <Guide />}
-      {simulationIsActive && <Simulation />}
+      <MenuDrawer />
+      <Outlet />
     </>
   );
 }
