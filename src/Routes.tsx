5670d247c13a6485ab625b4a2ca4d93178a6f132
Yuri Na <yuri.na938@gmail.com>
2025-04-04 02:54:38 -0400

    add home page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,13 +5,15 @@ import Simulation from "./components/Simulation";
 import ReferencePage from "./components/References";
 import ExplorePage from "./components/ExplorePage";
 import ForYouPage from "./components/ForYouPage";
+import HomePage from "./components/HomePage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "", element: <Guide /> },
+      { path: "", element: <HomePage /> },
+      { path: "guide", element: <Guide /> },
       { path: "simulation", element: <Simulation /> },
       { path: "references", element: <ReferencePage /> },
     ],
